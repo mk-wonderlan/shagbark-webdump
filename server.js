487e34c3205a5a8cce2113b0c465c4cfe49a5b24
ottoremse <otto.remse@gmail.com>
2014-08-25 15:11:37 +0200

    fixed port

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ app.post('/ircmessage',function(req, res)
     res.end();
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(80, function(){
+  console.log('listening on *:80');
 });
